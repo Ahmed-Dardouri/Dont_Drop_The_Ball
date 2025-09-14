622987ae05f18c6b42aae3020e7101e32a410284
abyss rover <ahmeddardouri6@gmail.com>
2025-09-14 21:22:12 +0200

    better controls: left move, right jump.

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -37,4 +37,4 @@ func _on_jump_touch_button_released() -> void:
 	
 	
 func hide_controls(game_over_event: GameOverEvent):
-	visible = true
+	visible = false
