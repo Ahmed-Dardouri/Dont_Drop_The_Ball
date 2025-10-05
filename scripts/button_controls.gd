94ba875903729b058415f0007e91d6d27cba02a7
abyss rover <ahmeddardouri6@gmail.com>
2025-10-05 19:54:45 +0200

    added OS name check

--- scripts/button_controls.gd
+++ scripts/button_controls.gd
@@ -7,10 +7,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Events.add_listener(GameOverEvent, hide_controls)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	Events.add_listener(WorldBuiltEvent, handle_world_built)
 
 
 func _on_right_touch_button_pressed() -> void:
@@ -37,7 +34,10 @@ func _on_jump_touch_button_released() -> void:
 	var move := Enums.PlayerMoves.JUMP 
 	Events.invoke(MoveEvent.new(move, false, move_power))
 	
-	
+
+func handle_world_built(event: WorldBuiltEvent):
+	if OS.get_name() == "Android" or OS.get_name() == "iOS":
+		visible = true
 	
 func hide_controls(game_over_event: GameOverEvent):
 	visible = false
